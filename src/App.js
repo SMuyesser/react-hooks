1c0928a3630038c03f8857fa6cfc1e315b65029b
SMuyesser <smuyesser@gmail.com>
2019-10-30 12:43:46 -0700

    adds counter button and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
   render () {
     return (
-      <h2>Counter App</h2>
+      <div>
+        <h2>Counter App</h2>
+        <button onClick={this.increment}>Clicked {this.state.count} times</button>
+      </div>
     );
-  }
-}
+  };
+};
 
 export default App;
